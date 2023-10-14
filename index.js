d9cdbf7639888f1f92bbd67fcebbcc8ec352afbb
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-14 21:03:03 +0530

    commit 2

--- index.js
+++ index.js
@@ -35,19 +35,19 @@ function removeElement(e){
         if(confirm('Are you sure to delete ?')){
             var li=e.target.parentElement;
            
-            var email=li.textContent.split(" - ")[1];
-            localStorage.removeItem(email);
+            var description=li.textContent.split(" - ")[1];
+            localStorage.removeItem(description);
             list.removeChild(li);
         }
     }
     else if(e.target.classList.contains('edit')){
         var li=e.target.parentElement;
         const arr=li.textContent.split(" - " );
-        var email=arr[1];
-        localStorage.removeItem(email);
+        var description=arr[1];
+        localStorage.removeItem(description);
         document.getElementById('id1').value=arr[0];
         document.getElementById('id2').value=arr[1];
-        document.getElementById('id3').value=arr[3];
+        document.getElementById('id3').value=arr[2];
         list.removeChild(li);
     }
 }
